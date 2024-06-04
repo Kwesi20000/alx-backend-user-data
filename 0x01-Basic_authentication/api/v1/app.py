69046d1b2270bbfc17da0d3179631bbca5ef7bae
Kwesi20000 <hendrickskumi@yahoo.com>
2024-06-04 12:16:06 -0700

    hendy

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python3
-"""Route module for the API.
 """
-import os
+Route module for the API
+"""
 from os import getenv
+from api.v1.views import app_views
 from flask import Flask, jsonify, abort, request
 from flask_cors import (CORS, cross_origin)
-
-from api.v1.views import app_views
+import os
 from api.v1.auth.auth import Auth
 from api.v1.auth.basic_auth import BasicAuth
 
@@ -22,36 +22,33 @@ if auth_type == 'basic_auth':
     auth = BasicAuth()
 
 
-@app.errorhandler(404)
-def not_found(error) -> str:
-    """the not found handler.
-    """
-    return jsonify({"error": "Not found"}), 404
-
-
 @app.errorhandler(401)
 def unauthorized(error) -> str:
-    """the unauthorized handler.
-    """
+    """unauthorized handler"""
     return jsonify({"error": "Unauthorized"}), 401
 
 
 @app.errorhandler(403)
 def forbidden(error) -> str:
-    """the forbidden handler.
-    """
+    """forbidden handler"""
     return jsonify({"error": "Forbidden"}), 403
+
+
+@app.errorhandler(404)
+def not_found(error) -> str:
+    """ Not found handler
+    """
+    return jsonify({"error": "Not found"}), 404
 
 
 @app.before_request
 def authenticate_user():
-    """this authenticates a user before processing a request.
-    """
+    """Authenticate a user before processing"""
     if auth:
         excluded_paths = [
             '/api/v1/status/',
             '/api/v1/unauthorized/',
-            '/api/v1/forbidden/',
+            '/api/v1/forbidden/'
         ]
         if auth.require_auth(request.path, excluded_paths):
             auth_header = auth.authorization_header(request)
